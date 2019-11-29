04ed15cba851c5311e31289b1b64814b9c1bf82d
sdnjsouza <54451375+sdnjsouza@users.noreply.github.com>
2019-11-29 00:05:56 -0300

    Update votacao.js

--- public/js/votacao.js
+++ public/js/votacao.js
@@ -1,58 +1,26 @@
 //https://www.senado.leg.br/atividade/const/con1988/con1988_03.07.2019/art_14_.asp
 //o voto é obrigatório para os cidadãos alfabetizados maiores de 18 anos e menores de 70 anos. 
-//No entanto, é facultado para os jovens a partir de 16 anos e para os acima de 70 anos. 
-
-/* let numero = 0, idade = 0;
-       let estado;
-       for (let l = 0; l < 2; l++) {
-           for (let c = 0; c < 3; c++) {
-               numero++;
-               document.write(numero);
-           }
-       }
-       document.write("<br>");
-        function teste(valor) {             
-           if(valor < 16 ){
-               let y ="ererer";
-           y = formvotacao.estado.value();
-             
-           }
-       }
-       for (let l = 0; l < 10; l++) {
-           document.write(" | ");
-           for (let c = 0; c < 20; c++) {
-               idade++;
-               document.write(idade);
-               document.write(" ");
-           }
-           document.write(" | <br> ");
-
-       }
-        var formvotacao = document.getElementById("form01");
-
-           var idade = formvotacao.idade.value;
-       
-           if(idade< 16){
-           
-        formvotacao.estado.value = "INAPTO!";
-          
-           }
-           document.getElementById("rrr").innerHTML = "Hello JavaScript!";
-console.log()
-       
-       */
+//No entanto, é facultado para os jovens a partir de 16 anos e para os acima de 70 anos.
 
 function votacao() {
+    
     //let dn = new Date(document.getElementById("nascimento").value) exibe  o dia aterior
-    dteste1 = document.getElementById("nascimento").value.split('-')
-    dteste2 = document.getElementById("votacao").value.split('-')
+    const dteste1 = document.getElementById("nascimento").value.split('-')
+    const dteste2 = document.getElementById("votacao").value.split('-')
 
     let dn = new Date(dteste1[0], dteste1[1], dteste1[2])
     let dv = new Date(dteste2[0], dteste2[1], dteste2[2])
 
-    if (dn > dv) {
-        //alert('Você nasceu hoje!?!?')
-        alert('Seja coerente com as datas!!!!')
+    console.log(dteste1)
+
+    if( dteste1[0] == ""){
+        alert('Selecione a data de nascimento')
+    }
+    if (dteste2[0] == "") {
+        alert('Selecione a data de votação')
+    }
+    if (dn.getDate == dv.getDate || dn > dv) {
+        alert('A data de nascimento deve ser menor que a data de votação')
     }
     if (dn < dv) {
         let idade = Math.floor(Math.ceil(Math.abs(dn.getTime() - dv.getTime()) / (1000 * 3600 * 24)) / 365.25)
@@ -65,21 +33,17 @@ function votacao() {
         } else if (idade <= 70) {
             document.getElementById("estado").value = "OBRIGATÓRIO"
         } else {
-            document.getElementById("estado").value = "INAPTO"
+            document.getElementById("estado").value = "FACULTATIVO"
         }
     }
 
     //document.getElementById("rrr").innerHTML = dn + " | Hello JavaScript! | " + dv;
-
-
-
     // console.log(hoje)
     // console.log(dn.getDate())
     //console.log(dv.getDate())
 }
 function testevotacao() {
-    //console.log("OK!")
-
+    
     let dv = new Date(2019, 10, 10)
     document.getElementById("votacao").innerHTML = 
     "  Data de votação testada: "+dv.getDate() + "/"+ dv.getMonth() + "/" + dv.getFullYear() 
@@ -97,7 +61,7 @@ function testevotacao() {
         } else if (idade <= 70) {
             situacao = "OBRIGATÓRIO"
         } else {
-            situacao = "INAPTO"
+            situacao = "FACULTATIVO"
         }
         console.log(" idade: " + idade+ " | Situação "+ situacao + " | "
         + dn.getFullYear() +"/"+dn.getMonth() + "/"+dn.getDate()
